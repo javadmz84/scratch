8c86ce62e5135fc9b3597b75ff78f893acd1bace
PoulStar Student <pstudent@localhost.localdomain>
2020-11-04 20:32:55 +0330

    a

---
+++ Term 2/4-6-iterate.py
@@ -0,0 +1,5 @@
+string = 'mn besyar adm bahali hastam'
+length = len(string)
+print(length)
+for i in range(length):
+    print (i, string[i])
---
+++ Term 2/4-square.py
@@ -0,0 +1,11 @@
+import turtle as tr
+
+
+tr.forward(30)
+tr.lt(90)
+tr.forward(30)
+tr.lt(90)
+tr.forward(30)
+tr.lt(90)
+tr.forward(30)
+tr.done()
