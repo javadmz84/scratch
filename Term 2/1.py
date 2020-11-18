469747007ad7f9073fa00dd5ee8442dbee1a482d
javadmz84 <momenzadehjavad11@gmail.com>
2020-11-18 20:32:52 +0330

    javad

---
+++ Term 2/1.py
@@ -0,0 +1,13 @@
+username = input('what is your user name?')
+password = input('what is your password?')
+if username == 'poulstar':
+
+    print('true')
+else:
+    print('dari estebah mizani!!!')
+if password == '123456':
+    print('true')
+else:
+    print('dari estebah mizani!!!')
+
+    
---
+++ Term 2/2.py
@@ -0,0 +1,11 @@
+a = 'AJisA'
+a = a.upper()
+print(a)
+
+a = a.lower()
+print(a)
+
+n = "89*"
+print(n.isnumeric())
+print(n.isalpha())
+
