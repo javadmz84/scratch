09d022e94bd1b662803f1d4e2d77541c77ad0929
javadmz84 <momenzadehjavad11@gmail.com>
2020-11-25 20:03:10 +0330

    taha

---
+++ Term 2/test of stringmethods.py
@@ -0,0 +1,10 @@
+# poulstar
+string1 = "@#$$^%^*Poulstar#@$%#$%#$%"
+new_string1 = string1.strip('@#$%*^').lower()
+print(new_string1)
+
+
+# poulstar
+string2 = "@#$$^%^*Poulstar#@$%#$%#$%"
+new_string2 = string2.split()[4].lower()
+print(new_string2)
---
+++ Term 2/test pop.py
@@ -0,0 +1,3 @@
+a = ['javad', 'iliya', 'setayesh', 'artin']
+a.pop(3)
+print(a)
