dd67db7e247f3acb858416867291e196baa2a3c9
javadmz84 <momenzadehjavad11@gmail.com>
2021-04-03 04:48:42 -0700

    kjhkj

--- تمرین/tkinter library.py/button.py
+++ تمرین/tkinter library.py/button.py
@@ -11,6 +11,5 @@ def javad():
 # دکمه را ساختیم اما کاری انجام نمیدهد پس برایش تابع میسازیم که در بالا ساختیم
 button = tk.Button(root, text='click me',command=javad, fg='blue', bg='green')
 button.pack()
-
-
+ll;kf;dls
 root.mainloop()
