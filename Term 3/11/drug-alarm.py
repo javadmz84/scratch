e02d3332cb2fb83d80df3f97a0b11f883cc5e3df
javadmz84 <momenzadehjavad11@gmail.com>
2021-04-14 08:58:51 -0700

    ghj

--- Term 3/11/drug-alarm.py
+++ Term 3/11/drug-alarm.py
@@ -1,27 +1,39 @@
-import tkinter as tk
+import tkinter as tk 
 import tkinter.ttk as ttk
 
 
 root = tk.Tk()
-
 note = ttk.Notebook(root)
 note.grid(row=0, column=0)
-#####################################3
+
 patients = ttk.Frame(note)
 note.add(patients, text='Patients')
-timers = ttk.Frame(note)
+
+timers= ttk.Frame(note)
 note.add(timers, text='Timers')
-##########################################
+##################First row of patients#################
 lf1 = tk.LabelFrame(patients, text='Patient 1')
 lf1.grid(row=0, column=0)
-
 tk.Label(lf1, text='Name').grid(row=0, column=0)
-tk.Label(lf1, text='Tiime').grid(row=1, column=0)
+tk.Label(lf1, text='Time').grid(row=1, column=0)
 name1 = tk.StringVar()
 time1 = tk.StringVar()
+
 tk.Entry(lf1, textvariable=name1).grid(row=0, column=1)
-tk.Entry(lf1, textvariable=time1).grid(row=1, column=1)
-#####################second row of patient####################
+##################Timer 1###############
+f1 = tk.Frame(lf1)
+f1.grid(row=1, column=1)
+h_p_l = tk.StringVar()
+h_p_l.set('12')
+m_p_l = tk.StringVar()
+m_p_l.set('30')
+s_p_l = tk.StringVar()
+s_p_l.set('30')
+tk.Spinbox(f1, from_=0, to=23, wrap=True, width=2, textvariable=h_p_l, state='readonly').grid(row=0, column=0)
+tk.Spinbox(f1, from_=0, to=59, wrap=True, width=2, textvariable=m_p_l, state='readonly').grid(row=0, column=1)
+tk.Spinbox(f1, from_=0, to=59, wrap=True, width=2, textvariable=s_p_l, state='readonly').grid(row=0, column=2)
+
+##################Second row of patients###################
 lf2 = tk.LabelFrame(patients, text='Patient 2')
 lf2.grid(row=1, column=0)
 tk.Label(lf2, text='Name').grid(row=0, column=0)
@@ -30,18 +42,49 @@ name2 = tk.StringVar()
 time2 = tk.StringVar()
 
 tk.Entry(lf2, textvariable=name2).grid(row=0, column=1)
-tk.Entry(lf2, texrvariable=time2).grid(row=1, column=1)
-##################Third row of patients#############
+##################Timer 2####################
+f2 = tk.Frame(lf2)
+f2.grid(row=1, column=1)
+h_p_2 = tk.StringVar()
+h_p_2.set('12')
+m_p_2 = tk.StringVar()
+m_p_2.set('30')
+s_p_2 = tk.StringVar()
+s_p_2.set('30')
+tk.Spinbox(f2, from_=0, to=23, wrap=True, width=2, textvariable=h_p_2, state='readonly').grid(row=0, column=0)
+tk.Spinbox(f2, from_=0, to=59, wrap=True, width=2, textvariable=m_p_2, state='readonly').grid(row=0, column=1)
+tk.Spinbox(f2, from_=0, to=59, wrap=True, width=2, textvariable=s_p_2, state='readonly').grid(row=0, column=2)
+##################Third row of patients###################
 lf3 = tk.LabelFrame(patients, text='Patient 3')
 lf3.grid(row=2, column=0)
 tk.Label(lf3, text='Name').grid(row=0, column=0)
 tk.Label(lf3, text='Time').grid(row=1, column=0)
 name3 = tk.StringVar()
 time3 = tk.StringVar()
-#####################################
+###################
 tk.Entry(lf3, textvariable=name3).grid(row=0, column=1)
-tk.Entry(lf3, textvariable=time3).grid(row=1, column=1)
+f3 = tk.Frame(lf3)
+f3.grid(row=1, column=1)
+h_p_3 = tk.StringVar()
+h_p_3.set('12')
+m_p_3 = tk.StringVar()
+m_p_3.set('30')
+s_p_3 = tk.StringVar()
+s_p_3.set('30')
+tk.Spinbox(f3, from_=0, to=23, wrap=True, width=2, textvariable=h_p_3, state='readonly').grid(row=0, column=0)
+tk.Spinbox(f3, from_=0, to=59, wrap=True, width=2, textvariable=m_p_3, state='readonly').grid(row=0, column=1)
+tk.Spinbox(f3, from_=0, to=59, wrap=True, width=2, textvariable=s_p_3, state='readonly').grid(row=0, column=2)
 
+##############First row of timer##################
+p1 = tk.StringVar()
+p1.set('Patient1')
+tk.Label(timers, textvariable=p1).grid(row=0, column=0, padx=10, pady=10)
 
-##########################################
+p2 = tk.StringVar()
+p2.set('Patient2')
+tk.Label(timers, textvariable=p2).grid(row=0, column=1,  pady=10)
+
+p3 = tk.StringVar()
+p3.set('Patient3')
+tk.Label(timers, textvariable=p3).grid(row=0, column=3, padx=10, pady=10)
 root.mainloop()
--- تمرین/tkinter library.py/calculator.py
+++ تمرین/tkinter library.py/calculator.py
@@ -2,33 +2,34 @@ from tkinter import *
 
 root=Tk()
 root.title('Simple Calculator')
-
-def button_click(number):
-    current = e.get()
-    e.delet(0, END)
-    e.insert (0, str(current) + str(number))
     
-
-
-
-
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 
-button_1 = Button(root, text='1', padx=40, pady=20, command=Lambda: button_click(1))
-button_2 = Button(root, text='2', padx=40, pady=20, command=Lambda: button_click(2))
-button_3 = Button(root, text='3', padx=40, pady=20, command=Lambda: button_click(3))
-button_4 = Button(root, text='4', padx=40, pady=20, command=Lambda: button_click(4))
-button_5 = Button(root, text='5', padx=40, pady=20, command=Lambda: button_click(5))
-button_6 = Button(root, text='6', padx=40, pady=20, command=Lambda: button_click(6))
-button_7 = Button(root, text='7', padx=40, pady=20, command=Lambda: button_click(7))
-button_8 = Button(root, text='8', padx=40, pady=20, command=Lambda: button_click(8))
-button_9 = Button(root, text='9', padx=40, pady=20, command=Lambda: button_click(9))
-button_0 = Button(root, text='0', padx=40, pady=20, command=Lambda: button_click(0))
-button_add = Button(root, text='+',padx=39, pady=20, command=Lambda: button_click )
-button_equal = Button(root, text='=', padx = 91, pady = 20, command=Lambda :button_click)
-button_clear = Button(root, text='Clear', padx = 79, pady = 20, command=Lambda: button_click)
+
+def button_click(number):
+    
+    current = e.get()
+    e.delete(0, END)
+    e.insert (0, str(current) + str(number))
+
+
+
+
+button_1 = Button(root, text='1', padx=40, pady=20, command= Lambda: button_click(1))
+button_2 = Button(root, text='2', padx=40, pady=20, command= Lambda:button_click(2))
+button_3 = Button(root, text='3', padx=40, pady=20, command= Lambda:button_click(3))
+button_4 = Button(root, text='4', padx=40, pady=20, command= Lambda:button_click(4))
+button_5 = Button(root, text='5', padx=40, pady=20, command= Lambda:button_click(5))
+button_6 = Button(root, text='6', padx=40, pady=20, command= Lambda:button_click(6))
+button_7 = Button(root, text='7', padx=40, pady=20, command= Lambda:button_click(7))
+button_8 = Button(root, text='8', padx=40, pady=20, command= Lambda:button_click(8))
+button_9 = Button(root, text='9', padx=40, pady=20, command= Lambda:button_click(9))
+button_0 = Button(root, text='0', padx=40, pady=20, command= Lambda:button_click(0))
+button_add = Button(root, text='+',padx=39, pady=20, command= button_click )
+button_equal = Button(root, text='=', padx = 91, pady = 20, command=button_click)
+button_clear = Button(root, text='Clear', padx = 79, pady = 20, command=button_click)
 
 # put the buttons on the screen
 
