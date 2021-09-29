2bb2d0dd68115a78b5ac823b36a39338f5e1482f
javadmz84 <momenzadehjavad11@gmail.com>
2021-09-29 09:50:52 -0700

    kjhkj

--- Term4/1/2/create_data.py
+++ Term4/1/2/create_data.py
@@ -5,7 +5,7 @@ con = pymysql.connect(host='localhost', user='root' , password='JaVaDmZ84!')
 
 try:
     with con.cursor() as cur:
-        cur.execute('CREATE DATABASE my_db_n')
+        cur.execute('CREATE DATABASE product')
 except:
     print('Access denied')
 
--- Term4/1/2/create_table.py
+++ Term4/1/2/create_table.py
@@ -1,14 +1,14 @@
 import pymysql
 
-con = pymysql.connect(host='localhost', user='root', password='JaVaDmZ84!')
+con = pymysql.connect(host='localhost', user='root', password='JaVaDmZ84!', database='product')
 
 
-sql = 'CREATE TABLE user(id int auto_increment, name varchar(255), primary key(id))'
+sql = 'CREATE TABLE TV(id int auto_increment, name varchar(255), primary key(id))'
 
 
 try:
     with con.cursor() as cur:
         cur.execute(sql)
 except:
-    print('Access denied!')
+        print('Access denied!')
     
