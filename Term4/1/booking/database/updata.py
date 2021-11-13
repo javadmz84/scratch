53e454fe94517344811ad9c34193bd523394355e
javadmz84 <momenzadehjavad11@gmail.com>
2021-11-13 06:02:12 -0800

    kllk

--- Term4/1/booking/database/updata.py
+++ Term4/1/booking/database/updata.py
@@ -1,12 +1,10 @@
 import sqlalchemy as db
 
 
-engine = db.create_engine('mysql+pymysql://root:JaVaDmZ84!@localhost:3306/booking')
 
-connection = engine.connect()
-metadata = db.MetaData()
-
-
-data = db.Table('person', metadata, autoload=True, autoload_with=engine)
-query = db.update(data).values(fullname='taha momenzadeh').where(data.c.id==1)
-ResultProxy = connection.execute(query)
+def update():
+    query = db.update(data).values(
+        fullname=search_fullname.get(),
+        phone=search_phone.get(),
+        ).where(data.c.idnum==res[0])
+    connection.execute(query)
--- Term4/1/booking/main.py
+++ Term4/1/booking/main.py
@@ -2,4 +2,5 @@ from database import functions
 
 
 #functions.rigister_person('javad momenzadeh ', '1')
-functions.set_fligh('rasht', 'tehran', '2019-12-26_11:51:00.516384', '2020-1-30_11:40:00.345434', 'Iran_air_to',120 , 100)
+#functions.set_fligh('rasht', 'mashhad', '2019-12-26_11:51:00.516384', '2020-1-30_11:40:00.345434', 'Iran_air_to',120 , 100)
+functions.bood_ticket(1,1)
